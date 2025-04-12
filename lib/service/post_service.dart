d841831a40e8d8a5cace09fca579741357b85c93
caodung227 <cdnh1234567890@gmail.com>
2025-04-13 00:49:56 +0700

    feat: get history posts

--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -106,4 +106,28 @@ class PostService {
       throw Exception('Failed to set privacy for post');
     }
   }
+
+  Future<List<PostModelResponse>> getHistoryPosts(int size, int page) async {
+    final response = await callApi(
+      "post-service/posts/history?size=$size&page=$page",
+      'GET',
+      isToken: true,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseMap =
+      jsonDecode(utf8.decode(response.bodyBytes));
+      final BaseResponseApi baseResponse =
+      BaseResponseApi.fromJson(responseMap);
+      if (baseResponse.code != 1000) {
+        throw Exception(baseResponse.message);
+      }
+      final List<PostModelResponse> posts = baseResponse.result['content']
+          .map((post) => PostModelResponse.fromJson(post))
+          .toList()
+          .cast<PostModelResponse>();
+      return posts;
+    } else {
+      throw Exception('Failed to get history posts');
+    }
+  }
 }
---
+++ lib/view_model/history_view_model.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/widgets.dart';
+import 'package:untitled/constants/appconstants.dart';
+import 'package:untitled/data/local/local_storage.dart';
+import 'package:untitled/model/comment_response.dart';
+import 'package:untitled/model/post_model_response.dart';
+import 'package:untitled/service/post_service.dart';
+
+class HistoryViewModel extends ChangeNotifier {
+  HistoryViewModel() {
+    scrollController.addListener(_onScroll);
+    fetchHistoryPosts();
+    _init();
+  }
+
+  final List<PostModelResponse> posts = [];
+  int _page = 0;
+  final int _size = 20;
+  bool _isLoading = false;
+  bool _hasMore = true;
+  String urlAvatar = AppConstants.urlImageDefault;
+  final ScrollController scrollController = ScrollController();
+
+  Future<void> fetchHistoryPosts({bool isRefresh = false}) async {
+    if (_isLoading || !_hasMore) return;
+
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      if (isRefresh) {
+        _page = 1;
+        posts.clear();
+        _hasMore = true;
+      }
+
+      final response =
+      await PostService.instance.getHistoryPosts(_size, _page);
+
+      if (response.isNotEmpty) {
+        posts.addAll(response);
+        _page++;
+      }
+
+      if (response.length < _size) {
+        _hasMore = false;
+      }
+    } catch (e) {
+      throw Exception("Failed to fetch history posts: $e");
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void _onScroll() {
+    if (scrollController.position.pixels >=
+        scrollController.position.maxScrollExtent - 200) {
+      fetchHistoryPosts();
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  bool get isLoading => _isLoading;
+  bool get hasMore => _hasMore;
+
+  _init() async {
+    urlAvatar = await LocalStorage.instance.userUrlAvatar ??
+        AppConstants.urlImageDefault;
+    notifyListeners();
+  }
+
+  Future<List<CommentResponse>> getComment(PostModelResponse post) async {
+    final response = await PostService.instance.getCommentById(post.id);
+    return response;
+  }
+}
